660885f41a71c198e0865b9618d7d641bf3e0d10
tegar kurniawan <tegarkurniawan145@gmail.com>
2015-11-03 15:26:57 +0700

    terbaru2

---
+++ resources/views/writemessage.blade.php
@@ -0,0 +1,21 @@
+@extends('app')
+
+@section('content')
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Send message</div>
+                    <form action="{{ route('send') }}" method="POST">
+                        <input type="text" name="message" >
+                        <input type="submit" value="send">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    
+@endsection
