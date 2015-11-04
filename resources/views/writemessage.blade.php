c183aae29751ec74444e40abc043b44076123ee3
tegar kurniawan <tegarkurniawan145@gmail.com>
2015-11-04 12:11:24 +0700

    larachat

--- resources/views/writemessage.blade.php
+++ resources/views/writemessage.blade.php
@@ -1,22 +1,55 @@
 @extends('app')
 
 @section('content')
-
+    <script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
+    <script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+    <script src="https://cdn.socket.io/socket.io-1.3.4.js"></script>
     <div class="container">
+        
         <div class="row">
             <div class="col-md-10 col-md-offset-1">
+             <div class="row">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Send message</div>
-                    <form action="{{ route('send') }}" method="POST">
-                        
+
+                    <div class="panel-heading">message</div>
+
+                    <!--<form action="{{ route('send') }}" method="POST">
                         <textarea name="message"></textarea> <br>
                         <input type="submit" value="send">
-                    </form>
+                    </form>-->
+                    <div class="panel-body">
+                        <div id="messages" ></div>
+                    </div>
+                    <div class="panel-footer">
+                        <input type="text" name="message" id="message_input"></input>
+                        <button onclick="chat.send();">Kirim</button>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+<script>
+    var socket = io.connect('http://localhost:8890');
 
+    socket.on('message', function (data) {
+        $( "#messages" ).append( "<p>"+data+"</p>" );
+    
+      });
+
+    var chat = {
+        send: function () {
+            $.ajax({
+                url: '{{ route('send') }}',
+                type: 'POST',
+                data: 'message='+$('#message_input').val(),
+                success: function (response) {
+                    $('#message_input').val('');
+                }
+            });
+        }
+    };
+
+</script>
 
     
 @endsection
